dcfb588e761eac3a08a223b3c9ede835c9548cf8
Rushi Khelage <rushikhelage2004@gmail.com>
2025-08-19 23:51:24 +0530

    resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -4,14 +4,10 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Uncomment these lines and run once in a Python shell to download NLTK data:
-# import nltk
-# nltk.download('punkt')
-# nltk.download('stopwords')
 
 def get_all_resumes(resume_folder="resumes"):
     resumes = {}
-    # Create folder if it doesn't exist
+
     if not os.path.exists(resume_folder):
         os.makedirs(resume_folder)
 
@@ -29,7 +25,7 @@ def get_all_resumes(resume_folder="resumes"):
 def extract_keywords(text, num_keywords=10):
     stop_words = set(stopwords.words('english'))
     words = word_tokenize(text.lower())
-    # Filter words: only alphabets and not stopwords
+    
     words = [w for w in words if w.isalpha() and w not in stop_words]
 
     freq_dist = nltk.FreqDist(words)
